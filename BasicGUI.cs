60f61a72a730e33c2a9478638c334676155e911d
Andre Dragu <andredragu@Andromeda.local>
2013-04-04 21:21:42 +0200

    Button toegevoegd

--- BasicGUI.cs
+++ BasicGUI.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 [ExecuteInEditMode]
 public class BasicGUI : MonoBehaviour {
-	
-	
-	
+
 	void OnGUI ()
 	{
 		GUI.contentColor = Color.red;
 		GUILayout.Label(gameObject.name);
+		if (GUILayout.Button(gameObject.name))
+			Debug.Log(gameObject.name);
 	}
 }
