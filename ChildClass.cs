596cee077a2821cf9c8643a2b540cb4361fafb7e
Andre Dragu <andredragu@Andromeda.local>
2013-04-05 15:21:08 +0200

    ChildClass toegevoegd

---
+++ ChildClass.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public class ChildClass : BaseClass {
+
+	// Use this for initialization
+	protected override void Start () {
+		myName = "Child";
+	}
+	
+}
